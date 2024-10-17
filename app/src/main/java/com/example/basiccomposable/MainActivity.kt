02118ff95376d16d1e7109e2cc9f61353f401190
irajesicajanuaris <jjiraaa1@gmail.com>
2024-10-17 21:10:35 +0700

    menambahkan fontstyle menjadi italic

--- app/src/main/java/com/example/basiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/basiccomposable/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -52,8 +53,7 @@ fun BasicComposable(ira: Modifier = Modifier) {
         Text(text = "Ini adalah halaman Login",
             style = TextStyle(
                 fontSize = 20.sp,
-
-            )
+                fontStyle = FontStyle.Italic)
         )
     }
 
