7d03f8fc0713174af4569132276c07ceab7e7ca2
irajesicajanuaris <jjiraaa1@gmail.com>
2024-10-17 20:13:01 +0700

    menghapus nama dan mengganti modifier menjadi Ira

--- app/src/main/java/com/example/basiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/basiccomposable/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -21,8 +23,7 @@ class MainActivity : ComponentActivity() {
             BasicComposableTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     BasicComposable(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        ira = Modifier.padding(innerPadding)
                     )
                 }
             }
