62f94740c12e1d97c9801c2172bc27dd9352236e
irajesicajanuaris <jjiraaa1@gmail.com>
2024-10-17 22:11:24 +0700

    menambahkan modifier agar bisa merubah size foto, padding, dan menggunakan clip untuk membuat sudut melengkung pada gambar

--- app/src/main/java/com/example/basiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/basiccomposable/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -84,8 +86,9 @@ fun BasicComposable(ira: Modifier = Modifier) {
 
         Image(
             painter =  painterResource(id = R.drawable.iraa),
-            contentDescription = null
-        )
+            contentDescription = null,
+            modifier = Modifier.size(500.dp)
+                .padding(20.dp) .clip(RoundedCornerShape(200.dp)))
     }
 
 
